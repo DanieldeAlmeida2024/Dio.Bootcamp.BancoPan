563c630416fc032eaaa61d5d3c8f6ae1d7af0222
Daniel de Almeida <danieldealmeidadev@outlook.com>
2025-01-10 19:22:46 -0300

    Aulas 1 a 4 sobre Aprendendo sintaxe Java

---
+++ aprendendo-sintaxe-java/src/br/edu/dio/anatomiadasclasses/RegrasDeCriacaoJava.java
@@ -0,0 +1,48 @@
+package br.edu.dio.anatomiadasclasses;
+
+public class RegrasDeCriacaoJava {
+    /*
+
+        - Praticar a clareza no nome dos arquivos, classes, métodos e variáveis.
+        sugestões:
+            - Variáveis devem ser claras, sem abreviações ou definição sem sentido.
+            - Uma variável deve ser sempre no singular, exceto quando se referir a um array ou coleção.
+            - Defina um idioma único para as variáveis.
+            -
+
+        - estáticos (static) identificam métodos, classes ou variáveis que não serão instanciados e
+        farão uma atividade que não necessita disso.
+
+        Criação de PROJETOS:
+            - Seguir a numenclatura clara do projeto
+            - Nome em letra minuscula, separado por ífem " - "
+
+        Criação de Classes:
+            - Nomenclatura clara da classe, Lembrar das regras e princípios SOLID
+            - O nome do arquivo .java deve ser exatamente o nome da classe
+            - Todas as palavras do nome devem ter a primeira letra MAIÚSCULA
+
+        Cliação de Variáveis:
+            - Toda variável deve ser escrita em letra minúscula, com excessão
+            caso tenha um nome composto, neste caso deve-se usar o padrão camelCase.
+            - Não identificar variáveis com caracteres, numeros e símbolos.
+            - Devem conter apenas letras, (_) underline, $ ou os números 0 a 9.
+            - Devem obrigatóriamente iniciar por uma letra(preferencialmente), _ ou $, jamais com número
+            - Não usar palavras-chave da linguagem, deve ser único no escopo, não conter espaços.
+            - Variáveis escritas em letra MAIÚSCULA são para identificar variáveis que não sofrerão alteração
+            no valor. ex.: BR = "Brasil", será sempre este o valor e não deve ser alterado. Também criada no tipo:
+            final String BR = "Brasil".
+
+            - Em Java, a declaração é feita da seguinte forma: "Tipo nomeDaVariavel = Atribuição ;"
+
+        Criação de Métodos:
+            - Visibilidade TipoDeRetorno nomeObjetivoNoInfinitivo Parâmetros.
+            ex.: public Double calcularSaldoEmDolar (Double saldo) {}, infinitivo: Somar, Multiplicar, Formatar.
+            - Nomenclatura seguir a convenção camelCase
+
+        Convenção de Subdiretórios:
+            - COMERCIAL: com. (com.hypertech.notification)
+            - ORGANIZACIONAL: org. (org.) (edu.daniel.tema.sustentabilidade ou edu.daniel.primeiraaula)
+            - OPENSOURCE: org. ou opensource.
+     */
+}
---
+++ aprendendo-sintaxe-java/src/br/edu/dio/tiposevariaveis/TiposVariaveis.java
@@ -0,0 +1,25 @@
+package br.edu.dio.tiposevariaveis;
+
+public class TiposVariaveis {
+    /*
+        Tipos Primitivos:
+            - int(4 bytes)
+            - byte(1 byte)
+            - short(2 bytes)
+            - long(8 bytes)
+            - float(4 bytes) não é mais muito usado por comprometer o uso em determinadas situações, só mantem uma precisão decimal de 6 a 7 digitos
+            - double(8 bytes)
+            - boolean
+            - char
+
+        Declaração de variável:
+            - Tipo nomeVariavel = AtribuiçãoDeValorOpcional
+            ex.: int idade;
+            ex.: int anoFabricacao = 2025;
+            ex.: double salarioMinimo = 1500;
+
+        Atribuição de valor:
+            - O tipo LONG precisa terminar com L e o tipo FLOAT precisa terminar com F
+
+     */
+}
